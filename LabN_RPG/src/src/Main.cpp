2d6fdd30c79ffd465a8d635a00fcbece748200f1
VingerGrint <badcode@internet.ru>
2022-11-24 20:57:55 +0300

    First commit

---
+++ LabN_RPG/src/src/Main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "SFML/Graphics.hpp"
+int main() 
+{
+	sf::RenderWindow window{ sf::VideoMode{640, 360}, "Hello world!" };
+	while (window.isOpen())
+	{
+		sf::Event event{};
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+			{
+				window.close();
+			}
+		}
+
+		window.clear();
+		window.display();
+	}
+	return 0;
+}
